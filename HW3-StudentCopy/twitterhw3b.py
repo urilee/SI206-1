d0e7c23f9e7b6f88aadbba651ce3b9daf0b70841
Uriel Lee <uriellee@umich.edu>
2016-11-08 23:58:20 -0500

    search term

--- HW3-StudentCopy/twitterhw3b.py
+++ HW3-StudentCopy/twitterhw3b.py
@@ -6,7 +6,28 @@
 # 3) Print the average polarity of the results
 
 # Be prepared to change the search term during demo.
+import tweepy
+from textblob import TextBlob
 
+# Unique code from Twitter
 
-print("Average subjectivity is")
-print("Average polarity is")
+# Boilerplate code here
+auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
+auth.set_access_token(access_token,access_token_secret)
+
+api = tweepy.API(auth)
+#Now we can Create Tweets, Delete Tweets, and Find Twitter Users
+avgsubjectivity = 0.0
+avgpolarity = 0.0
+
+public_tweets = api.search('minimal tattoos')
+
+for tweet in public_tweets:
+	print(tweet.text)
+	analysis = TextBlob(tweet.text)
+	avgsubjectivity += analysis.sentiment.subjectivity
+	avgpolarity += analysis.sentiment.polarity
+	#print(analysis.sentiment)
+
+print("Average subjectivity is " + str(avgsubjectivity))
+print("Average polarity is " + str(avgpolarity))
