0394f545ce23d9e90d98756e1e5a4b2680556a5b
Uriel Lee <uriellee@umich.edu>
2016-11-08 23:57:17 -0500

    update with media

--- HW3-StudentCopy/twitterhw3a.py
+++ HW3-StudentCopy/twitterhw3a.py
@@ -4,5 +4,20 @@
 
 # You will demo this live for grading.
 
+import tweepy
+
+# Unique code from Twitter
+
+# Boilerplate code here
+auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
+auth.set_access_token(access_token,access_token_secret)
+
+api = tweepy.API(auth)
+
+twittermessage = "a picture from Copenhagen! #UMSI-206 #Proj3"
+photofile = "Asset0011.jpg"
+api.update_with_media(photofile, twittermessage)
+
 print("""No output necessary although you 
 	can print out a success/failure message if you want to.""")
+
