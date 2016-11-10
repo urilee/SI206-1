bb0616cc19f857a037455e783f120b8cceeb7e9d
Uriel Lee <uriellee@umich.edu>
2016-11-09 23:47:06 -0500

    slight change

--- HW3-StudentCopy/twitterhw3a.py
+++ HW3-StudentCopy/twitterhw3a.py
@@ -8,6 +8,7 @@ import tweepy
 
 # Unique code from Twitter
 
+
 # Boilerplate code here
 auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
 auth.set_access_token(access_token,access_token_secret)
