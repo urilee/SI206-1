ed43b184c116f34c2a4a43df2ed95f3032eebd71
Uriel Lee <uriellee@umich.edu>
2016-11-10 23:46:02 -0500

    finished bs

--- HW3-StudentCopy/bshw3.py
+++ HW3-StudentCopy/bshw3.py
@@ -11,6 +11,8 @@
 # Deliverables
 # Make sure the new page is uploaded to your GitHub account.
 
+#References:
+#(1) = http://stackoverflow.com/questions/15056633/python-find-text-using-beautifulsoup-then-replace-in-original-soup-variable
 
 import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
@@ -19,20 +21,23 @@ import re
 url = 'http://collemc.people.si.umich.edu/data/bshw3StarterFile.html'
 html = urllib.request.urlopen(url).read()
 soup = BeautifulSoup(html, 'html.parser')
-new_string = str(soup)
-html2 = soup.prettify("utf-8")
 
+#(1)
+#find student & replace with AMAZING student
 for new_changes in soup.find_all(text = re.compile('student')):
-	new_changes.replace("students", "AMAZING students")
+	new_string = str(new_changes)
+	new_string = new_string.replace("student", "AMAZING student")
+	new_changes.replace_with(new_string)
 
+#find image and change also
 for image in soup.find_all("img", src = True):
 	if image.get('src') == "https://testbed.files.wordpress.com/2012/09/bsi_exposition_041316_192.jpg":
 		image['src'] = "IMG_5778.jpg"
 	else:
 		image['src'] = "media/logo.png"
 
-
+html = soup.prettify("utf-8")
 f = open("bshw3.html", "wb")
-f.write(html2)
-
+f.write(html)
+f.close()
 
